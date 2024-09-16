5fe1e0ca99c2e4eddfd7e3cfce81b8f26cecce51
Diegolo03 <diego.atila099@gmail.com>
2024-09-16 13:50:16 +0200

    mas

--- Assets/Scripts/Ejercicio3_2.cs
+++ Assets/Scripts/Ejercicio3_2.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ejercicio3_1 : MonoBehaviour
+public class Ejercicio3_2 : MonoBehaviour
 {
-    [SerializeField] int numero1, numero2;
+    [SerializeField] string nombre;
     // Start is called before the first frame update
     void Start()
     {
-        
-        Debug.Log("Numero 1: " + numero1);
-        Debug.Log("Numero 2: " + numero2);
+
+        Debug.Log("Bienvenido de nuevo, " + nombre);
+       
 
     }
 
