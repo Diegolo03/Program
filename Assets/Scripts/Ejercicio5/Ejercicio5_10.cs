b13a0954dfa151b74e3e958f33ee3471099dda3c
Diegolo03 <diego.atila099@gmail.com>
2024-09-25 13:58:49 +0200

    cambios

--- Assets/Scripts/Ejercicio5/Ejercicio5_10.cs
+++ Assets/Scripts/Ejercicio5/Ejercicio5_10.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class Ejercicio5_10 : MonoBehaviour
 {
+    [SerializeField] int tp;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (tp <= 10)
+        {
+            Debug.Log("El clima es: Frío");
+        }
+        else if (tp > 10 && tp <= 20)
+        {
+            Debug.Log("El clima es: Nublado");
+        }
+        else if (tp > 20 && tp <= 30)
+        {
+            Debug.Log("El clima es: Caluroso");
+        }
+        else if (tp > 30)
+        {
+            Debug.Log("El clima es: Tropical");
+        }
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Ejercicio6/Ejercicio6.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio6 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Para el ejercicio 6.1:
+
+        //a) true
+        //b) true
+        //c) true
+        //d) false
+        //e) true
+        //f) true
+
+        //Para el ejercicio 6.2:
+
+        //a) true
+        //b) true
+        //c) true
+        //d) false
+        //e) true
+        //f) false
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
