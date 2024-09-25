28c9116fc119557cd31302b5f9e5d71707e01ced
Diegolo03 <diego.atila099@gmail.com>
2024-09-25 14:10:55 +0200

    7_4

---
+++ Assets/Scripts/Ejercicio7/Ejercicio7_4.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio7_4 : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int numero = 100; numero > 0; numero--)
+        {
+
+
+            Debug.Log(numero);
+        }
+;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
