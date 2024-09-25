15d00ae7c8cd8f82c5afbe7b6243da926b7d5a38
Diegolo03 <diego.atila099@gmail.com>
2024-09-25 11:58:31 +0200

    Update Ejercicio5_8.cs

--- Assets/Scripts/Ejercicio5/Ejercicio5_8.cs
+++ Assets/Scripts/Ejercicio5/Ejercicio5_8.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class Ejercicio5_8 : MonoBehaviour
 {
+    [SerializeField] int horas, minutos, segundos;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        if (horas >= 0 && horas < 24 && minutos >=0 && minutos <60 && segundos >= 0 && segundos < 60 )
+        {
+            Debug.Log("La hora es correcta, son las: " + horas + ":" + minutos + ":" + segundos + ".");
 
+        }
+        else
+        {
+            Debug.Log("La hora es incorrecta");
+        }
+
+    }
     // Update is called once per frame
     void Update()
     {
