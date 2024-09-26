e18a48adf056db75a61a4828c6d59fff6c3ccbeb
Diegolo03 <diego.atila099@gmail.com>
2024-09-27 00:52:18 +0200

    revisar el 7.11

--- Assets/Scripts/Ejercicio7/Ejercicio7_11.cs
+++ Assets/Scripts/Ejercicio7/Ejercicio7_11.cs
@@ -1,26 +1,33 @@
+using Palmmedia.ReportGenerator.Core;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Ejercicio7_11 : MonoBehaviour
 {
-    
+    [SerializeField] int numero;
+    int numero1 = 1;
     // Start is called before the first frame update
     void Start()
     {
-        for (int numero = 0; numero <= 100; numero++)
+        
+        while (numero1 <= numero)
         {
-
-            
-            Debug.Log(numero);
+            Debug.Log(numero1);
+            numero1++;
         }
-
-
+        
+        for (int i = numero1; i <= numero; i++)
+        {
+            Debug.Log(i);
+        }
     }
-
     // Update is called once per frame
     void Update()
     {
+
         
     }
 }
