624887324c91e9cbb1fb473370fab259c1c1dad7
Diegolo03 <diego.atila099@gmail.com>
2024-09-27 14:21:22 +0200

    10

--- Assets/Scripts/Ejercicio7/Ejercicio7_10.cs
+++ Assets/Scripts/Ejercicio7/Ejercicio7_10.cs
@@ -1,26 +1,69 @@
+using Palmmedia.ReportGenerator.Core;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Ejercicio7_10 : MonoBehaviour
 {
-    int numero = 101;
     // Start is called before the first frame update
+    [SerializeField] int numero0, numero1;
     void Start()
     {
-        while (numero <= 101 && numero > 1)
+        
+
+        Debug.Log("Números entre " + numero0 + " y " + numero1 + " son:");
+        if (numero0 > numero1)
         {
-
-            numero--;
-            Debug.Log(numero);
+            for (int i = numero0; i <= numero1; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    Debug.Log(i + " par");
+                }
+                else
+                {
+                    Debug.Log(i + " impar");
+                }
+                if (i > 0)
+                {
+                    Debug.Log(i + " positivo");
+                }
+                else
+                {
+                    Debug.Log(i + " negativo");
+                }
+            }
         }
-
-
+        else
+        {
+            for (int i = numero1; i >= numero0; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    Debug.Log(i + " par");
+                }
+                else
+                {
+                    Debug.Log(i + " impar");
+                }
+                if (i > 0)
+                {
+                    Debug.Log(i + " positivo");
+                }
+                else
+                {
+                    Debug.Log(i + " negativo");
+                }
+            }
+        }
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+
+
+
     }
+
 }
